ff3c0f09c5781b93b5eb901d8f43a172569d1421
nelbr <nwaissman@yahoo.com.br>
2025-02-03 21:29:34 +0100

    Add 2 menu scenes, add music, control music volume from options menu

---
+++ Assets/Scripts/MenuPlay.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPlay: MonoBehaviour
+{
+    public void PlayGame() 
+    {
+        SceneManager.LoadSceneAsync("GameScene");
+    }
+
+    public void SetSounds()
+    {
+        SceneManager.LoadSceneAsync("SettingsMenu");
+    }
+
+    public void QuitGame() 
+    {
+        Application.Quit();
+    }
+
+    public void BackMain()
+    {
+        SceneManager.LoadSceneAsync("MainMenu");
+    }    
+}
