898bdf53419f7936c11f90835632126b49bff1a9
nelbr <nwaissman@yahoo.com.br>
2025-02-12 23:02:21 +0100

    Fix FX Slider to control SFX volume

--- Assets/Scripts/VolumeSettings.cs
+++ Assets/Scripts/VolumeSettings.cs
@@ -6,15 +6,23 @@ public class VolumeSettings : MonoBehaviour
 {
     [SerializeField] private AudioMixer myMixer;
     [SerializeField] private Slider MusicSlider;
+    [SerializeField] private Slider FXSlider;
 
     private void Start()
     {
         SetMusicVolume();
+        SetFXVolume();
     }
     public void SetMusicVolume()
     {
         float volume = MusicSlider.value;
         myMixer.SetFloat("Music", Mathf.Log10(volume)*20);
+    }
+
+    public void SetFXVolume()
+    {
+        float volume = FXSlider.value;
+        myMixer.SetFloat("SFX", Mathf.Log10(volume)*20);
     }
 }
 
