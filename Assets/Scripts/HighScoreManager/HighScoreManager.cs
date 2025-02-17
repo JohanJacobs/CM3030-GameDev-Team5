22a6d4b491d3530b70066c82194d7ed9d8f85b37
Johan Jacobs <20131902+JohanJacobs@users.noreply.github.com>
2025-02-17 16:39:08 +1300

    fixed duplicating singleton.

--- Assets/Scripts/HighScoreManager/HighScoreManager.cs
+++ Assets/Scripts/HighScoreManager/HighScoreManager.cs
@@ -5,26 +5,22 @@ using UnityEngine;
 
 public class HighScoreManager :MonoBehaviour
 {
-    public static HighScoreManager Instance { get; private set; }
-
-    private int lastHighScore=0;
-
-    private void Awake()
+    private static HighScoreManager _instance;
+    public static HighScoreManager Instance
     {
-
-        if (Instance != this && Instance != null)
-        { 
-            // somehow this is a second instance ?
-            Destroy(this);
-            return;
-        }
-        else
+        get
         {
-            Instance = this;
-            DontDestroyOnLoad(this.gameObject);
+            if (!_instance)
+            {
+                _instance = new GameObject().AddComponent<HighScoreManager>();
+                _instance.name = "HighScoreManager";
+                DontDestroyOnLoad(_instance.gameObject);
+            }
+            return _instance;
         }
     }
 
+    private int lastHighScore=0;
     public void AddNewScore(int score)
     {
         if (score > lastHighScore)
