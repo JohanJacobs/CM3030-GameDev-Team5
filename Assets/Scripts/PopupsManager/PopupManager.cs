7e3e807d0f99da077e6a80b1e9a9c0f5525fd7a3
Johan Jacobs <20131902+JohanJacobs@users.noreply.github.com>
2025-02-22 10:55:54 +1300

    set offsets between multiple attributes

--- Assets/Scripts/PopupsManager/PopupManager.cs
+++ Assets/Scripts/PopupsManager/PopupManager.cs
@@ -31,15 +31,21 @@ public class PopupManager : MonoBehaviour
     #region Pickup Callbacks
     private void Pickup_PickedUp(object sender, GameObject target)
     {
+        // handle XP messages
         if (sender.GetType().ToString() == "ExperienceOrbPickup")
             CreateNewPopup(target.transform.position, "+1 XP", target.transform, _popupTimeToLive);
 
+        // pickups that impact the players abilities
         if (sender.GetType().ToString() == "PickupWithEffect")
         {
+            // create a message for each attribute that is changed
             var pwe = sender as PickupWithEffect;
+            int attribute_counter = 0;
+            var message_offset = new Vector3(0f, -0.5f, 0f);
             foreach (var e in pwe.Effect.Modifiers)
             {
-                CreateNewPopup(target.transform.position, $"+{e.Value} {e.Attribute.ToString()}", target.transform, _popupTimeToLive);
+                CreateNewPopup(target.transform.position - (message_offset * attribute_counter ), $"+{e.Value} {e.Attribute.ToString()}", target.transform, _popupTimeToLive);
+                attribute_counter += 1;
             }
 
         }
