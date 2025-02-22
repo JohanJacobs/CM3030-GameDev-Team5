a9fa71a43f46fde21a2e8017adba424d48820bb5
Johan Jacobs <20131902+JohanJacobs@users.noreply.github.com>
2025-02-22 19:05:00 +1300

    create text box for abilities

---
+++ Assets/Scripts/AbilityTextOrientation.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ *  This class orients text inline with the angle to the player required to see the text in the game world
+ */
+public class AbilityTextOrientation : MonoBehaviour
+{
+    Transform _playerTransform;
+    Transform _cameraTransform;
+    private void Awake()
+    {
+        _playerTransform = FindObjectOfType<Player>().transform;
+        _cameraTransform = Camera.main.transform;
+    }
+
+
+    private void LateUpdate()
+    {
+        UpdateOrientation();
+    }
+
+
+    private void UpdateOrientation()
+    {
+        var vector_from_player_to_camera = _cameraTransform.position - _playerTransform.position;
+
+        var look_at_position = transform.position + vector_from_player_to_camera;
+        transform.LookAt(look_at_position);
+        transform.RotateAround(transform.position, transform.up, 180f);
+    }
+}
