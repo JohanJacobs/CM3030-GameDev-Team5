025a73115cd8c5c31e849a255931171ee8c4a397
Johan Jacobs <20131902+JohanJacobs@users.noreply.github.com>
2025-02-28 19:48:53 +1300

    Level up player at XP intervals

---
+++ Assets/Scripts/LevelUpSystem/LevelUpSystem.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml.XPath;
+using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
+
+public class LevelUpSystem : MonoBehaviour
+{
+    [SerializeField] int xpInterval=50;
+    [SerializeField] Effect xpEffect;
+
+    Player _player;
+    AbilitySystemComponent _abilitySystem;
+    
+    private void Awake()
+    {
+        _player = GetComponent<Player>();
+        _abilitySystem = GetComponent<AbilitySystemComponent>();
+    }
+
+    private void OnEnable()
+    {
+        Pickup.GlobalPickedUp += Pickup_GlobalPickedUp;
+    }
+
+    private void OnDisable()
+    {
+        Pickup.GlobalPickedUp -= Pickup_GlobalPickedUp;
+    }
+
+    private void Pickup_GlobalPickedUp(Pickup pickup, GameObject pickedUpBy)
+    {
+        if (pickup is ExperienceOrbPickup)
+        {
+            if (_player.Experience % xpInterval == 0)
+            {
+                _abilitySystem.ApplyEffectToSelf(xpEffect);                
+            }
+        }
+    }
+}
