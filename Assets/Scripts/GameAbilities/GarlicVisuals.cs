58477256db7d01dfbde665e6dab4f634ba3b25e6
nelbr <nwaissman@yahoo.com.br>
2025-03-08 09:55:52 +0100

    last minute comments and readme

--- Assets/Scripts/GameAbilities/GarlicVisuals.cs
+++ Assets/Scripts/GameAbilities/GarlicVisuals.cs
@@ -1,1 +1,13 @@
+/*
+
+University of London
+BsC Computer Science Course
+Games Design
+Final Assignment - Streets of Fire Game
+
+Group 5 
+
+Please view README file for detailed information
+
+*/
 ﻿
--- Assets/Scripts/LevelUpSystem/LevelUpEffect.cs
+++ Assets/Scripts/LevelUpSystem/LevelUpEffect.cs
@@ -1,3 +1,15 @@
+/*
+
+University of London
+BsC Computer Science Course
+Games Design
+Final Assignment - Streets of Fire Game
+
+Group 5 
+
+Please view README file for detailed information
+
+*/
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
